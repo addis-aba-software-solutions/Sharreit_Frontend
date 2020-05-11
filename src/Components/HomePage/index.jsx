baf9e8ba81b3cc7c2756363676997dc3ac88ab14
Yohannes Berhanu <Johnlights51@gmail.com>
2020-05-11 17:05:21 +0300

    After Color Change

--- src/Components/HomePage/index.jsx
+++ src/Components/HomePage/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Typography, Box, Button, AppBar, Grid } from "@material-ui/core";
+import {
+  Typography,
+  Box,
+  Button,
+  AppBar,
+  Popover,
+  Grid,
+} from "@material-ui/core";
 import HomePageHeader from "./components/HomePageHeader";
 import classes from "./styles";
 import AllItems from "./components/itemView";
 
 export default function Landing() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <>
       <AppBar position="fixed">
@@ -12,13 +32,8 @@ export default function Landing() {
       </AppBar>
       <Box style={classes.bodySpacer} />
 
-      <Grid
-        xs={12}
-        align="center"
-        style={classes.HeaderSecondRow}
-        spacing={5}
-      >
-        <Button style={classes.Headertext2}>
+      <Grid xs={12} align="center" style={classes.HeaderSecondRow} spacing={5}>
+        <Button onClick={handleClick} style={classes.Headertext2}>
           <Grid item>product Sharing</Grid>
         </Button>
         <Button style={classes.Headertext2}>
@@ -28,6 +43,7 @@ export default function Landing() {
           <Grid item>Digital Sharing</Grid>
         </Button>
       </Grid>
+
 
       <Box style={classes.root}>
         <Grid container spacing={2} xs={12}>
@@ -75,6 +91,23 @@ export default function Landing() {
           </Grid>
         </Grid>
       </Box>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <AllItems />
+      </Popover>
     </>
   );
 }
